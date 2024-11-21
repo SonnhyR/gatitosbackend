9e40b03020a70f9ae4f90bb4b90a08c444935357
sumimeow <ziursonnhy@gmail.com>
2024-11-21 18:48:13 -0400

    primera act

---
+++ src/main/java/com/kittens/backend/kittensbackend/KittensbackendApplication.java
@@ -0,0 +1,14 @@
+package com.kittens.backend.kittensbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittensbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittensbackendApplication.class, args);
+		System.err.println("holjjj");
+	}
+
+}
---
+++ src/test/java/com/kittens/backend/kittensbackend/KittensbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kittens.backend.kittensbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittensbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
